f536638c3ca8a5a44f5cf2bd3d0016be4ddf511b
liaoyuyu <1146521145@qq.com>
2018-10-31 16:50:24 +0800

    Update taochuhui/js/collection.js

--- taochuhui/js/collection.js
+++ taochuhui/js/collection.js
@@ -96,20 +96,6 @@ $(function(){
     // $("body,html").on('touchmove', function(e) {
     //     e.preventDefault();
     // });
-    let self = this;
-    document.addEventListener('touchstart', function (e) {
-        self.moveY = e.targetTouches[0].pageY;
-    })
-    document.addEventListener('touchmove', function (e){
-        e.preventDefault();
-        let moveWidth = self.moveY - e.targetTouches[0].pageY;
-        if (moveWidth !== 0) {
-            document.body.scrollTop += moveWidth;
-        }
-    })
-    document.addEventListener('touchmove', function (e) {
-        e.preventDefault();
-    })
 
 
     function move_box(){
