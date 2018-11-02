9c40aa2ab9c245f8118cf88d3cba7a2d2f13bd2c
liaoyuyu <1146521145@qq.com>
2018-11-02 18:07:03 +0800

    Update taochuhui/js/order.js

--- taochuhui/js/order.js
+++ taochuhui/js/order.js
@@ -17,50 +17,50 @@ $(function(){
     alert(2)
 
 
-    // new Vue({
-    //     el:".order",
-    //     data:{
-    //         data:"",
-    //     },
-    //     created:function(){
-    //         alert(3)
-    //     },
-    //     mounted:function(){
-    //         this.get_url_index()
-    //     },
-    //     methods:{
-    //         // 获取链接地址传过来的值
-    //         get_url_index:function(){
-    //             var url = document.URL
-    //             console.log(url)
-    //             url = url.substr(url.lastIndexOf("?")+1,)
-    //             alert(url)
+    new Vue({
+        el:".order",
+        data:{
+            data:"",
+        },
+        created:function(){
+            alert(3)
+        },
+        mounted:function(){
+            this.get_url_index()
+        },
+        methods:{
+            // 获取链接地址传过来的值
+            get_url_index:function(){
+                var url = document.URL
+                console.log(url)
+                url = url.substr(url.lastIndexOf("?")+1,)
+                alert(url)
 
-    //             this.click_nav(url)
-    //         },
-    //         // 切换导航样式 和 数据
-    //         click_nav:function(index){
-    //             // 改变当前URL 参数
-    //             var target_url = window.location.href; 
-    //             var Now_url = (target_url.substr(0,target_url.lastIndexOf("?")+1)+index);
-    //             // console.log(Now_url)
-    //             history.pushState({},"",Now_url)
+                this.click_nav(url)
+            },
+            // 切换导航样式 和 数据
+            click_nav:function(index){
+                // 改变当前URL 参数
+                var target_url = window.location.href; 
+                var Now_url = (target_url.substr(0,target_url.lastIndexOf("?")+1)+index);
+                // console.log(Now_url)
+                history.pushState({},"",Now_url)
 
-    //             for(var i = 0;i<$(".order_li").length;i++){
-    //                 $(".order_li").eq(i).removeClass("order_nav_select")
-    //             }
-    //             $(".order_li").eq(index).addClass("order_nav_select")
+                for(var i = 0;i<$(".order_li").length;i++){
+                    $(".order_li").eq(i).removeClass("order_nav_select")
+                }
+                $(".order_li").eq(index).addClass("order_nav_select")
 
-    //             // 请求数据
-    //             // get_data("","Advertisement/GetWebIndexData","get").then(function(res){
+                // 请求数据
+                // get_data("","Advertisement/GetWebIndexData","get").then(function(res){
     
-    //             // },function(err){
-    //             //     alert("系统错误，请联系客服！")
-    //             //     // console.log(err)
-    //             // })
-    //         },
-    //     },
-    // })
+                // },function(err){
+                //     alert("系统错误，请联系客服！")
+                //     // console.log(err)
+                // })
+            },
+        },
+    })
 
 
 
