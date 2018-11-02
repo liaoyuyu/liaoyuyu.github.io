255d544a9631ff2d42f0a92303ac33ba3f742a81
liaoyuyu <1146521145@qq.com>
2018-11-02 17:15:51 +0800

    Update taochuhui/js/order_details.js

--- taochuhui/js/order_details.js
+++ taochuhui/js/order_details.js
@@ -1,9 +1,12 @@
 $(function(){
 
+
     $(".copy").click(function(){
         var input = $("#order_code_input")
         input.select()
         document.execCommand("Copy");
         console.log(document.execCommand("Copy"))
     })
+
+    
 })
