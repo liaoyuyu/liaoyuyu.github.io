5cb81739c723333c4706c9a56599b9f82c8d944f
liaoyuyu <1146521145@qq.com>
2018-11-02 15:48:51 +0800

    add

---
+++ taochuhui/js/order_details.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    $(".copy").click(function(){
+        var input = $("#order_code_input")
+        input.select()
+        document.execCommand("Copy");
+        console.log(document.execCommand("Copy"))
+    })
+
+})
