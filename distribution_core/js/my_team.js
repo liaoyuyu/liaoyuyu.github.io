0feb4c9eaefaeaf8eff74d1986475fb129af2eb6
liaoyuyu <1146521145@qq.com>
2018-12-18 16:15:48 +0800

    add

---
+++ distribution_core/js/my_team.js
@@ -0,0 +1,8 @@
+$(function(){
+
+    var a = "13667619032"
+    var b = set_center_asterisk(a)
+    // console.log(b)
+
+
+})
---
+++ distribution_core/js/public.js
@@ -0,0 +1,8 @@
+
+// 手机号中间设置星号
+function set_center_asterisk(obj){
+    var tel = obj;
+    var reg = /^(\d{3})\d{5}(\d{3})$/;
+    tel = tel.replace(reg, "$1*****$2");
+    return tel
+}
