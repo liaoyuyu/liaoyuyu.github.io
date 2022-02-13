a57ec3db45c16370e0f6c56c35ccfe623cf801ee
liaoyuyu <39960687+liaoyuyu@users.noreply.github.com>
2022-02-13 18:53:40 +0800

    Delete taochuhui directory

--- taochuhui/js/base_details.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-
-
-    $(".down_display").click(function(){
-        $(".base_introduce_pre").removeClass("base_introduce_pre_default")
-        $(".down_display").addClass("none")
-    })
-
-})
--- taochuhui/js/service.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-
-    $(".problem_title").click(function(){
-        $(this).siblings(".solve").toggle()
-        $(this).find(".problem_img").toggleClass("problem_img_down")
-    })
-
-
-})
