c7a4cfeb0071b0c567b479a2817e27304e4b7e0a
liaoyuyu <39960687+liaoyuyu@users.noreply.github.com>
2022-02-13 18:53:25 +0800

    Delete distribution_core directory

--- distribution_core/js/my_team.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-
-    var a = "13667619032"
-    var b = set_center_asterisk(a)
-    // console.log(b)
-
-
-})
--- distribution_core/js/service.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-
-    $(".problem_title").click(function(){
-        $(this).siblings(".solve").toggleClass("solve_down")
-        $(this).find(".front_arrow").toggleClass("problem_img_down")
-    })
-
-
-})
